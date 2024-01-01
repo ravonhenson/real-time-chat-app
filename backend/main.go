13f66e262a0b9bdad59d84ab27a642bcd43559d9
Ravon Henson <ravonhenson@gmail.com>
2024-01-01 18:11:38 -0500

    server operational

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,78 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// this works for now....
+		// but a security check is necessary later on
+		return true
+	},
+}
+
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the chat room.")
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			fmt.Println(err)
+			delete(clients, conn)
+			return
+		}
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				fmt.Println(err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+
+	}
+}
+
 func main() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", handleConnections)
 
-	fmt.Println("Hello world!")
+	go handleMessages()
 
+	fmt.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic("Error starting server: " + err.Error())
+	}
 }
